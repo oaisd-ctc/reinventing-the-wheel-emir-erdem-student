065df5eb5cdcfad071df5d74663b5a5b5659ad8c
GavinLoucksStudent <112421432+GavinLoucksStudent@users.noreply.github.com>
2023-01-19 19:43:00 +0000

    nice

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -2,6 +2,84 @@ namespace MyUtilities
 {
     public static class StringUtils
     {
-        
+        public static string ToUpper(string input)
+        {
+            string output ="";
+            for(int i = 0; i < input.Length; i++)
+            {
+                if()
+            }
+        }
+
+        public static string ToLower()
+        {
+
+        }
+
+        public static int Length(string input)
+        {
+            int output = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                output++;
+            }
+            return output;
+        }
+
+        public static string Reverse()
+        {
+
+        }
+
+        public static int CountVowels(string input)
+        {
+            int output = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' )
+                {
+                    output ++;
+                }
+                else
+                {
+
+                }
+                return output;
+            }
+        }
+
+        public static int CountConsonants(string input)
+        {
+            int output = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' )
+                {
+                    
+                }
+                else
+                {
+                    output++;
+                }
+                return output;
+            }
+        }
+
+        public static bool HasLetter(String Word, Char Letter)
+        {
+           
+            for(int i = 0; i < Word.Length; i++)
+            {
+                if(Letter == Word[i])
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+                
+            }
+        }
     }
 }
