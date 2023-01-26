19aa74197ca45982f785237aa5eac2bd7b3bf4aa
GavinLoucksStudent <112421432+GavinLoucksStudent@users.noreply.github.com>
2023-01-26 17:57:48 +0000

    nuget packages

--- Utils/StringUtils.cs
+++ Utils/StringUtils.cs
@@ -1,166 +1,166 @@
-namespace StringUtilities
-{
-    public static class StringUtils
-    {
-        public static string ToUpper(string input)
-        {
-            string output = "";
-            for (int i = 0; i < input.Length; i++)
-            {
-                if ()
-                {
+// namespace StringUtilities
+// {
+//     public static class StringUtils
+//     {
+//         public static string ToUpper(string input)
+//         {
+//             string output = "";
+//             for (int i = 0; i < input.Length; i++)
+//             {
+//                 if ()
+//                 {
 
-                }
-            }
-        }
+//                 }
+//             }
+//         }
 
-        public static string ToLower()
-        {
+//         public static string ToLower()
+//         {
 
-        }
+//         }
 
-        public static int Length(string input)
-        {
-            int output = 0;
-            for (int i = 0; i < input.Length; i++)
-            {
-                output++;
-            }
-            return output;
-        }
+//         public static int Length(string input)
+//         {
+//             int output = 0;
+//             for (int i = 0; i < input.Length; i++)
+//             {
+//                 output++;
+//             }
+//             return output;
+//         }
 
-        static string Reverse(string input)
-        {
-            int length = input.Length - 1;
-            string reversie = "";
-            char[] array = new char[input.Length];
-            while (length >= 0)
-            {
+//         static string Reverse(string input)
+//         {
+//             int length = input.Length - 1;
+//             string reversie = "";
+//             char[] array = new char[input.Length];
+//             while (length >= 0)
+//             {
 
-                reversie = reversie + input[length];
-                length--;
-            }
-            return reversie;
-        }
+//                 reversie = reversie + input[length];
+//                 length--;
+//             }
+//             return reversie;
+//         }
 
-        public static int CountVowels(string input)
-        {
-            int output = 0;
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
-                {
-                    output++;
+//         public static int CountVowels(string input)
+//         {
+//             int output = 0;
+//             for (int i = 0; i < input.Length; i++)
+//             {
+//                 if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
+//                 {
+//                     output++;
 
-                }
+//                 }
 
-            }
-            return output;
-        }
+//             }
+//             return output;
+//         }
 
-        public static int CountConsonants(string input)
-        {
-            int output = 0;
-            for (int i = 0; i < input.Length; i++)
-            {
-                if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
-                {
+//         public static int CountConsonants(string input)
+//         {
+//             int output = 0;
+//             for (int i = 0; i < input.Length; i++)
+//             {
+//                 if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'o' || input[i] == 'u' || input[i] == 'A' || input[i] == 'E' || input[i] == 'I' || input[i] == 'O' || input[i] == 'U')
+//                 {
 
-                }
-                else
-                {
-                    output++;
-                }
+//                 }
+//                 else
+//                 {
+//                     output++;
+//                 }
 
-            }
-            return output;
-        }
+//             }
+//             return output;
+//         }
 
-        public static bool HasLetter(String Word, Char Letter)
-        {
+//         public static bool HasLetter(String Word, Char Letter)
+//         {
 
-            int hasletter = 0;
-            bool letterItHas = false;
-            for (int i = 0; i < Word.Length; i++)
-            {
-                if (Letter == Word[i])
-                {
-                    hasletter++;
-                }
+//             int hasletter = 0;
+//             bool letterItHas = false;
+//             for (int i = 0; i < Word.Length; i++)
+//             {
+//                 if (Letter == Word[i])
+//                 {
+//                     hasletter++;
+//                 }
 
-            }
-            if (hasletter > 0)
-            {
-                letterItHas = true;
+//             }
+//             if (hasletter > 0)
+//             {
+//                 letterItHas = true;
 
-            }
-            return letterItHas;
+//             }
+//             return letterItHas;
 
-        }
+//         }
 
-        // The following methods are the additional methods
+//         // The following methods are the additional methods
 
-        public static string spaceOut(string word)
-        {
-            string nice = "";
-            foreach (char letter in word)
-            {
-                nice = nice + letter + " ";
-            }
-            return nice;
-        }
-        public static void printDown(string word)
-        {
-            foreach (char letter in word)
-            {
-                Console.WriteLine(letter);
-            }
-        }
-        public static bool Compare(string word, string word2)
-        {
-            int letterAmount = 0;
-            if (word.Length != word2.Length)
-            {
-                return false;
-            }
-            for (int i = 0; i < word.Length; i++)
-            {
+//         public static string spaceOut(string word)
+//         {
+//             string nice = "";
+//             foreach (char letter in word)
+//             {
+//                 nice = nice + letter + " ";
+//             }
+//             return nice;
+//         }
+//         public static void printDown(string word)
+//         {
+//             foreach (char letter in word)
+//             {
+//                 Console.WriteLine(letter);
+//             }
+//         }
+//         public static bool Compare(string word, string word2)
+//         {
+//             int letterAmount = 0;
+//             if (word.Length != word2.Length)
+//             {
+//                 return false;
+//             }
+//             for (int i = 0; i < word.Length; i++)
+//             {
 
-                if (word[i] == word2[i])
-                {
-                    letterAmount++;
-                }
+//                 if (word[i] == word2[i])
+//                 {
+//                     letterAmount++;
+//                 }
 
-            }
-            if (letterAmount == word.Length)
-            {
-                return true;
+//             }
+//             if (letterAmount == word.Length)
+//             {
+//                 return true;
 
-            }
-            else
-            {
-                return false;
-            }
+//             }
+//             else
+//             {
+//                 return false;
+//             }
 
 
-        }
-        public static string Remove(string word, char letterRemove)
-        {
+//         }
+//         public static string Remove(string word, char letterRemove)
+//         {
             
-            string newWord = "";
-            foreach(char letter in word)
-            {
-                if(letter == letterRemove)
-                {
-                    newWord = newWord + "";
-                }
-                else
-                {
-                    newWord += letter;
-                }
+//             string newWord = "";
+//             foreach(char letter in word)
+//             {
+//                 if(letter == letterRemove)
+//                 {
+//                     newWord = newWord + "";
+//                 }
+//                 else
+//                 {
+//                     newWord += letter;
+//                 }
                 
-            }
-            return newWord;
-        }
-    }
-}
+//             }
+//             return newWord;
+//         }
+//     }
+// }
