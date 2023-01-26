b44834151ab9b1827432c638646b3359099f480b
GavinLoucksStudent <112421432+GavinLoucksStudent@users.noreply.github.com>
2023-01-26 19:29:51 +0000

    Testing Son

---
+++ Utils/Tests.cs
@@ -0,0 +1,51 @@
+using Xunit;
+using StringUtilities;
+public class Tests
+{
+    [Fact]
+
+    public void Some_Very_Specific_Test_Use_Case_Here()
+    {
+        string testString = "Gavin";
+
+        //LengthTest
+        // int lengthStringOutcome = 5;
+        // Assert.Equal(lengthStringOutcome, StringUtils.Length(testString));
+
+        //ReverseTest
+        // string reverseOutcome = "nivaG";
+        // Assert.Equal(reverseOutcome, StringUtils.Reverse(testString));
+
+        //CountVowels
+        // int VowelAmount = 2;
+        // Assert.Equal(VowelAmount, StringUtils.CountVowels(testString));
+
+        //CountConsonants
+        // int ConsonantAmount = 3;
+        // Assert.Equal(ConsonantAmount, StringUtils.CountConsonants(testString));
+
+        //HasLetter
+        // bool HasLette = true;
+        // char LetterHas = 'a';
+        // Assert.Equal(HasLette, StringUtils.HasLetter(testString, LetterHas));
+
+        //spaceOut
+        // string spaceOutNice = "G a v i n ";
+        // Assert.Equal(spaceOutNice, StringUtils.spaceOut(testString));
+
+        //printDown
+        // char letterEnding = 'i';
+        // bool theEnder = true;
+        // Assert.Equal(theEnder, StringUtils.EndsWith(testString, letterEnding));
+
+        //Compare
+        // string comparison = "Gavin";
+        // bool comp = true;
+        // Assert.Equal(comp, StringUtils.Compare(testString, comparison));
+
+        //RemoveTest
+        // string removeOutcome = "Gvin";
+        // char removeThing = 'a';
+        // Assert.Equal(removeOutcome, StringUtils.Remove(testString, removeThing));
+    }
+}
