9cc64e774ca5c1c5dbf50e57007c3570fdc72b91
emir-erdem-student <112420889+emir-erdem-student@users.noreply.github.com>
2023-01-26 19:29:36 +0000

    XML Math Done

--- Utils/MathUtils.cs
+++ Utils/MathUtils.cs
@@ -27,6 +27,14 @@ namespace MathUtilities
         }
         public static double Min(double x, double y)
         {
+            ///<summary>
+            ///Finds the minimum of the two
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The minimum between the two. 
+            ///</returns>
             if (x < y)
             {
                 return x;
@@ -42,6 +50,14 @@ namespace MathUtilities
         }
         public static float Min(float x, float y)
         {
+            ///<summary>
+            ///Finds the minimum of the two
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The minimum between the two. 
+            ///</returns>
             if (x < y)
             {
                 return x;
@@ -57,6 +73,14 @@ namespace MathUtilities
         }
         public static int Max(int x, int y)
         {
+            ///<summary>
+            ///Finds the max of the two
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The maximum between the two. 
+            ///</returns>
             if (x > y)
             {
                 return x;
@@ -72,6 +96,14 @@ namespace MathUtilities
         }
         public static double Max(double x, double y)
         {
+            ///<summary>
+            ///Finds the max of the two
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The maximum between the two. 
+            ///</returns>
             if (x > y)
             {
                 return x;
@@ -87,6 +119,14 @@ namespace MathUtilities
         }
         public static float Max(float x, float y)
         {
+            ///<summary>
+            ///Finds the max of the two
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The maximum between the two. 
+            ///</returns>
             if (x > y)
             {
                 return x;
@@ -102,54 +142,157 @@ namespace MathUtilities
         }
         public static int Add(int x, int y)
         {
+            ///<summary>
+            ///Finds the sum of two numbers
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The sum of the two. 
+            ///</returns>
             return x + y;
         }
         public static double Add(double x, double y)
         {
+            ///<summary>
+            ///Finds the sum of two numbers
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The sum of the two. 
+            ///</returns>
             return x + y;
         }
         public static float Add(float x, float y)
         {
+            ///<summary>
+            ///Finds the sum of two numbers
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The sum of the two. 
+            ///</returns>
             return x + y;
         }
         public static int Subtract(int x, int y)
         {
+            ///<summary>
+            ///Finds the result of two numbers being subtracted
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The result of the first number being subtracted by the second. 
+            ///</returns>
             return x - y;
         }
         public static double Subtract(double x, double y)
         {
+            ///<summary>
+            ///Finds the result of two numbers being subtracted
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The result of the first number being subtracted by the second. 
+            ///</returns>
             return x - y;
         }
         public static float Subtract(float x, float y)
         {
+            ///<summary>
+            ///Finds the result of two numbers being subtracted
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///The result of the first number being subtracted by the second. 
+            ///</returns>
             return x - y;
         }
         public static int Multiply(int x, int y)
         {
+            ///<summary>
+            ///Finds the product of the two numbers
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///Product of the formula x * y 
+            ///</returns>
             return x * y;
         }
         public static double Multiply(double x, double y)
         {
+            ///<summary>
+            ///Finds the product of the two numbers
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///Product of the formula x * y
+            ///</returns> 
             return x * y;
         }
         public static float Multiply(float x, float y)
         {
+            ///<summary>
+            ///Finds the product of the two numbers
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///Product of the formula x * y
+            ///</returns> 
             return x * y;
         }
         public static int Divide(int x, int y)
         {
+            ///<summary>
+            ///Finds the result of the two numbers once divided
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///Result of the formula x / y
+            ///</returns> 
             return x / y;
         }
         public static double Divide(double x, double y)
         {
+            ///<summary>
+            ///Finds the result of the two numbers once divided
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///Result of the formula x / y
+            ///</returns> 
             return x / y;
         }
         public static float Divide(float x, float y)
         {
+            ///<summary>
+            ///Finds the result of the two numbers once divided
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The second number.</param>
+            ///<returns>
+            ///Result of the formula x / y
+            ///</returns> 
             return x / y;
         }
         public static int AbsValue(int x)
         {
+            ///<summary>
+            ///Finds the absolute value of a number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Result of |x|
+            ///</returns>
             if (x < 0)
             {
                 return x * -1;
@@ -161,6 +304,13 @@ namespace MathUtilities
         }
         public static double AbsValue(double x)
         {
+            ///<summary>
+            ///Finds the absolute value of a number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Result of |x|
+            ///</returns>
             if (x < 0)
             {
                 return x * -1;
@@ -172,6 +322,13 @@ namespace MathUtilities
         }
         public static float AbsValue(float x)
         {
+            ///<summary>
+            ///Finds the absolute value of a number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Result of |x|
+            ///</returns>
             if (x < 0)
             {
                 return x * -1;
@@ -183,6 +340,14 @@ namespace MathUtilities
         }
         public static int Power(int x, int y)
         {
+            ///<summary>
+            ///Finds the result of the number raised to the nth power
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The exponent.</param>
+            ///<returns>
+            ///Result of the formula x^y 
+            ///</returns>
             int result = 1;
             if (y > 0)
             {
@@ -202,6 +367,14 @@ namespace MathUtilities
         }
         public static double Power(double x, double y)
         {
+            ///<summary>
+            ///Finds the result of the number raised to the nth power
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The exponent.</param>
+            ///<returns>
+            ///Result of the formula x^y 
+            ///</returns>
             double result = 1;
             if (y > 0)
             {
@@ -222,6 +395,14 @@ namespace MathUtilities
         }
         public static float Power(float x, float y)
         {
+            ///<summary>
+            ///Finds the result of the number raised to the nth power
+            ///</summary>
+            ///<param name ="x">The first number.</param>
+            ///<param name ="y">The exponent.</param>
+            ///<returns>
+            ///Result of the formula x^y 
+            ///</returns>
             float result = 1;
             if (y > 0)
             {
@@ -242,18 +423,46 @@ namespace MathUtilities
 
         public static int Round(float x)
         {
+            ///<summary>
+            ///Finds whether to round up or down
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded number
+            ///</returns>
             return (int)x;
         }
         public static int Round(double x)
         {
+            ///<summary>
+            ///Finds whether to round up or down
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded number
+            ///</returns>
             return (int)x;
         }
         public static int Round(decimal x)
         {
+            ///<summary>
+            ///Finds whether to round up or down
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded number
+            ///</returns>
             return (int)x;
         }
         public static decimal Sqrt(decimal number)
         {
+            ///<summary>
+            ///Finds the square root of the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the root
+            ///</returns>
             decimal temp = 0;
             decimal sqrt = number / 2;
 
@@ -266,6 +475,13 @@ namespace MathUtilities
         }
         public static double Sqrt(double number)
         {
+            ///<summary>
+            ///Finds the square root of the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the root
+            ///</returns>
             double temp = 0;
             double sqrt = number / 2;
 
@@ -280,53 +496,137 @@ namespace MathUtilities
         // The following methods are the additional methods
         public static int Floor(double number)
         {
+            ///<summary>
+            ///Finds the result of rounding down the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded down number
+            ///</returns>
             return (int)number;
         }
         public static int Floor(float number)
         {
+            ///<summary>
+            ///Finds the result of rounding down the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded down number
+            ///</returns>
             return (int)number;
         }
         public static int Floor(decimal number)
         {
+            ///<summary>
+            ///Finds the result of rounding down the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded down number
+            ///</returns>
             return (int)number;
         }
         public static int Ceiling(double number)
         {
+            ///<summary>
+            ///Finds the result of rounding up the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded up number
+            ///</returns>
             double answer = number + 1;
             return (int)answer;
         }
         public static int Ceiling(float number)
         {
+            ///<summary>
+            ///Finds the result of rounding up the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded up number
+            ///</returns>
             double answer = number + 1;
             return (int)answer;
         }
         public static int Ceiling(decimal number)
         {
+            ///<summary>
+            ///Finds the result of rounding up the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the rounded up number
+            ///</returns>
             decimal answer = number + 1;
             return (int)answer;
         }
         public static int Square(int x)
         {
+            ///<summary>
+            ///Finds the result of squaring the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the squared number
+            ///</returns>
             return x * x;
         }
         public static double Square(double x)
         {
+            ///<summary>
+            ///Finds the result of squaring the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the squared number
+            ///</returns>
             return x * x;
         }
         public static float Square(float x)
         {
+            ///<summary>
+            ///Finds the result of squaring the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the squared number
+            ///</returns>
             return x * x;
         }
         public static float Cube(float x)
         {
+            ///<summary>
+            ///Finds the result of cubing the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the cubed number
+            ///</returns>
             return x * x * x;
         }
         public static double Cube(double x)
         {
+            ///<summary>
+            ///Finds the result of cubing the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the cubed number
+            ///</returns>
             return x * x * x;
         }
         public static int Cube(int x)
         {
+            ///<summary>
+            ///Finds the result of cubing the number
+            ///</summary>
+            ///<param name ="x">The input.</param>
+            ///<returns>
+            ///Returns the cubed number
+            ///</returns>
             return x * x * x;
         }
 
